f5e4376a3162331d9a7a4e265a12a31e38aa269f
BusyBox <2894415997@qq.com>
2023-05-03 18:59:02 +0800

    init

---
+++ build-WoCaoGMS-Qt_6_4_2_for_macOS-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles/WoCaoGMS_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/WoCaoGMS_autogen.dir/ParseCache.txt"
+  "WoCaoGMS_autogen"
+  )
+endif()
